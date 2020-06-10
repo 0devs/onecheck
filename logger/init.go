af27c4e2ded5a54c3e4baa4195e0dd39c2dffb26
alekzonder <alekzonder@gmail.com>
2020-06-10 09:57:08 +0700

    chg: logger package

---
+++ logger/init.go
@@ -0,0 +1,34 @@
+package logger
+
+import (
+	"encoding/json"
+	"go.uber.org/zap"
+)
+
+func InitLogger() *zap.Logger {
+	rawJSON := []byte(`{
+	  "level": "debug",
+	  "encoding": "console",
+	  "outputPaths": ["stdout"],
+	  "errorOutputPaths": ["stderr"],
+	  "encoderConfig": {
+	    "messageKey": "message",
+	    "levelKey": "level",
+	    "levelEncoder": "lowercase"
+	  }
+	}`)
+
+	var cfg zap.Config
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		panic(err)
+	}
+
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	zap.ReplaceGlobals(logger)
+
+	return logger
+}
--- main.go
+++ main.go
@@ -1,35 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/0devs/onecheck/cmd"
-	"go.uber.org/zap"
+	"github.com/0devs/onecheck/logger"
 )
 
 func main() {
-	rawJSON := []byte(`{
-	  "level": "debug",
-	  "encoding": "console",
-	  "outputPaths": ["stdout"],
-	  "errorOutputPaths": ["stderr"],
-	  "encoderConfig": {
-	    "messageKey": "message",
-	    "levelKey": "level",
-	    "levelEncoder": "lowercase"
-	  }
-	}`)
-
-	var cfg zap.Config
-	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
-		panic(err)
-	}
-
-	logger, err := cfg.Build()
-	if err != nil {
-		panic(err)
-	}
-
-	zap.ReplaceGlobals(logger)
-
+	logger.InitLogger()
 	cmd.Execute()
 }
